6430c17edd5cfe0a41c930780d37b581d1c2d800
josefcech8 <josefcech8@gmail.com>
2014-12-21 17:49:10 +0100

    frame classes repair

--- MathTournament/src/GraphicalUserInterface.java
+++ MathTournament/src/GraphicalUserInterface.java
@@ -1,6 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
 
-public class GraphicalUserInterface {
+public class GraphicalUserInterface extends JFrame {
+
+    private int screenSizeX, screenSizeY, frameSizeX, frameSizeY;
+    private Dimension screenSize;
+
     public GraphicalUserInterface() {
-        new FrameSettings();
+        super("Matematický turnaj");
+
+        frameSizeX = 1000;
+        frameSizeY = 600;
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        screenSizeX = screenSize.width;
+        screenSizeY = screenSize.height;
+        setBounds(screenSizeX/2-frameSizeX/2, screenSizeY/2-frameSizeY/2, frameSizeX, frameSizeY);
+
+        setVisible(true);
     }
 }
