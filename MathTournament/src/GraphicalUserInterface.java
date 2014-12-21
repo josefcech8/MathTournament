dcf26ba666177fdddf48ee86b96c5d549004d899
josefcech8 <josefcech8@gmail.com>
2014-12-21 20:10:17 +0100

    create panel

--- MathTournament/src/GraphicalUserInterface.java
+++ MathTournament/src/GraphicalUserInterface.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GraphicalUserInterface extends JFrame {
 
@@ -14,12 +16,32 @@ public class GraphicalUserInterface extends JFrame {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
+        getContentPane().setLayout(new CardLayout(0, 0));
 
         screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         screenSizeX = screenSize.width;
         screenSizeY = screenSize.height;
-        setBounds(screenSizeX/2-frameSizeX/2, screenSizeY/2-frameSizeY/2, frameSizeX, frameSizeY);
+        setBounds(screenSizeX / 2 - frameSizeX / 2, screenSizeY / 2 - frameSizeY / 2, frameSizeX, frameSizeY);
+
+        initLoginPanel();
 
         setVisible(true);
     }
+
+    //region login
+    JLabel labelTitle;
+    JPanel panelLogin;
+
+    private void initLoginPanel() {
+        panelLogin = new JPanel();
+        getContentPane().add(panelLogin);
+        panelLogin.setLayout(null);
+
+        labelTitle = new JLabel("Matematický turnaj");
+        labelTitle.setBounds(445, 20, 110, 20);
+        panelLogin.add(labelTitle);
+
+        add(panelLogin);
+    }
+    //endregion
 }
